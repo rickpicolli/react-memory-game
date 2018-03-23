758fe3d2a99a7a9a71b86c7708cb076d40a1f97a
rickpicolli <ricardopicolli@gmail.com>
2018-03-23 00:04:23 -0700

    updating

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -15,27 +15,7 @@ export default class Game extends React.Component {
     };
   }
 
-// const App = () => (
-//   <Wrapper>
-//     <PokemonCard
-//       image={pokemons[0].image}
-//     />
-//     <PokemonCard
-//       image={pokemons[1].image}
-//     />
-//     <PokemonCard
-//       image={pokemons[2].image}
-//     />
-//   </Wrapper>
-// );
-
-
-//shuffled.foreach => (card)
-
-
-
-
-render() {
+  render() {
 		return (
 			<div>
         <Navbar
@@ -43,20 +23,66 @@ render() {
           highestScore={this.state.highestScore}
         />
         <Wrapper className="col-md-12">
-        <div className="col-md-12">
-          {this.state.pokemons.map(pokemon =>{
+        <div className="jumbotron">
+        <h2>Welcome to Pokemon Memory game!</h2>
+        <h2> To play, you just need to click on the images below. Be carefull to not click at the same image twice.</h2>
+        <h2> Have Fun!!!</h2>
+      </div>
+          {this.state.pokemons.map((pokemon, index) =>{
             return <PokemonCard
-              key={pokemon.id}
+              key={index}
               image={pokemon.image}
               id={pokemon.id}
               name={pokemon.name}
+              index={index}
+              handleClick={(index) => {
+                this.handleClick(index);
+              }}
               />
               }
             )
           }
-          </div>
         </Wrapper>
 			</div>
 		)
   }
+
+  shuffle(a) {
+    //looping and moving objects from being next to eachother to shuffle/sorting
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+  }
+
+  handleClick (index) {
+    let {pokemons, score, prevScore} = this.state;
+
+
+
+    if(!pokemons[index].clicked) {
+      pokemons[index].clicked = true;
+      score++;
+    } else {
+      pokemons = pokemons.map((pokemon, index) => {
+        pokemon.clicked = false;
+        return pokemon;
+      });
+      prevScore = score > prevScore ? score : prevScore;
+      score = 0;
+    }
+
+    pokemons = this.shuffle(pokemons)
+
+    this.setState({
+      pokemons, score, prevScore
+    })
+
+  }
 }
+
+
+
+
+
